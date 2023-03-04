3944fa6568c6c7d6df38be4e8fe905ca12e223c9
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-03-03 21:13:45 -0500

    working routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -9,7 +9,9 @@ router.get('/', async (req, res) => {
 
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product }],
+      // include: [
+      //   { model: Product }
+      // ]
     });
     res.status(200).json(categoryData);
   } catch (err) {
@@ -17,21 +19,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/:id', (req, res) => {
-//   // find one category by its `id` value
-//   // be sure to include its associated Products
-// });
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
 
-// router.post('/', (req, res) => {
-//   // create a new category
-// });
+router.post('/', (req, res) => {
+  // create a new category
+});
 
-// router.put('/:id', (req, res) => {
-//   // update a category by its `id` value
-// });
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
 
-// router.delete('/:id', (req, res) => {
-//   // delete a category by its `id` value
-// });
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
 
 module.exports = router;
