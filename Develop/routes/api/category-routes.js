bd03c0c01abd960a88dc3c32ce4b88ee0f629b86
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-03-06 13:54:47 -0500

    cat put route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -52,17 +52,16 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.findById(req.params.id);
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
   
     if(!categoryData) {
       return res.status(404).json({message: 'Category not found.'})
     }
-
-    categoryData.name = req.body.name;
-    categoryData.description = req.body.description;
-    const updateCategory = await categoryData.save();
-  
-    req.json(updateCategory);
+    req.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -58,9 +58,13 @@ router.put('/:id', async (req, res) => {
             id: req.params.id,
       },
     });
+
+    if(!tagData) {
+      return res.status(404).json({message: 'Tag not found.'})
+    }
     res.status(200).json(tagData);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(400).json(err)
   }
 });
 
